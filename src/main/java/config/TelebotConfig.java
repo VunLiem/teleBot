7f16df1219137f59c3e6a1d41c5e7e59d0679677
VUN LIEM <96492677+VunLiem@users.noreply.github.com>
2022-07-20 10:17:00 +0700

    Update TelebotConfig.java

--- src/main/java/config/TelebotConfig.java
+++ src/main/java/config/TelebotConfig.java
@@ -1,10 +1,6 @@
 package config;
 
 public class TelebotConfig {
-//    public static final String BOT_TOKEN = "5210269411:AAEK2D4Tkqkh-Aoy3x63yjmrEt2BDP540rk";
-//    public static final String BOT_USERNAME = "My12tele_Bot";
-//    public static final String BOT_TOKEN = "5364421343:AAEATRGZud2KIicgvWRxBW31OURN07erqb8";
-//    public static final String BOT_USERNAME = "My12TeleBot";
-    public static final String BOT_TOKEN = "5425849788:AAH1-r3Homyb0uaxaNNMLlssk9ItdfRpjxI";
+    public static final String BOT_TOKEN = "Put your token api here";
     public static final String BOT_USERNAME = "Metfone_chatbot";
 }
